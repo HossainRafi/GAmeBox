2c008482173af1a392781fa922d237f319681d82
HossainRafi <hossainrafi71@gmail.com>
2023-06-14 21:57:03 +0600

    design search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo1.png";
+
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 
 //!================ Function Starts From Here =================
@@ -6,9 +7,13 @@ function Header() {
   return (
     <div>
       <img src={logo} width={60} height={60} />
-      <div>
+      <div className="flex bg-slate-200 mx-5 w-full p-2 rounded-full items-center px-2">
         <HiOutlineMagnifyingGlass />
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Search Games"
+          className="bg-transparent w-full outline-none pl-2 items-center rounded-full"
+        />
       </div>
     </div>
   );
