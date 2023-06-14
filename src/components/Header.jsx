43d79ecae21da599bc6a73b1539d45ef1ca0b381
HossainRafi <hossainrafi71@gmail.com>
2023-06-14 22:16:11 +0600

    design icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import logo from "./../assets/logo1.png";
 import { HiMoon, HiOutlineMagnifyingGlass, HiSun } from "react-icons/hi2";
 
 //!================ Function Starts From Here =================
 function Header() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="flex items-center p-3">
       <img src={logo} width={60} height={60} />
@@ -15,8 +18,14 @@ function Header() {
         />
       </div>
       <div>
-        <HiMoon />
-        <HiSun />
+        {toggle ? (
+          <HiMoon className="text-[35px] cursor-pointer bg-gray-200 text-black p-1 rounded-full" />
+        ) : (
+          <HiSun
+            className="text-[35px] cursor-pointer
+       bg-gray-200 text-black p-1 rounded-full "
+          />
+        )}
       </div>
     </div>
   );
