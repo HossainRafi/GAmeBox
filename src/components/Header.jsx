e4e6c3b3e5ca5acaff7163e38bce3a8a64a8b75b
HossainRafi <hossainrafi71@gmail.com>
2023-06-14 22:09:11 +0600

    dark light icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 import logo from "./../assets/logo1.png";
-
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { HiMoon, HiOutlineMagnifyingGlass, HiSun } from "react-icons/hi2";
 
 //!================ Function Starts From Here =================
 function Header() {
   return (
-    <div>
+    <div className="flex items-center p-3">
       <img src={logo} width={60} height={60} />
-      <div className="flex bg-slate-200 mx-5 w-full p-2 rounded-full items-center px-2">
+      <div className="flex bg-slate-200 mx-5 w-full p-2 rounded-full items-center px-3">
         <HiOutlineMagnifyingGlass />
         <input
           type="text"
-          placeholder="Search Games"
+          placeholder="Search Your Favourite Games"
           className="bg-transparent w-full outline-none pl-2 items-center rounded-full"
         />
+      </div>
+      <div>
+        <HiMoon />
+        <HiSun />
       </div>
     </div>
   );
