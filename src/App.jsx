dac16d3975725a8bfcd317cd4ec799603606e397
HossainRafi <hossainrafi71@gmail.com>
2023-06-15 22:03:07 +0600

    appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
-import { ThemeContext } from "./context/ThemeContext";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { createContext, useEffect, useState } from "react";
+
+import Home from "./Pages/Home";
+import { ThemeContext } from "./Context/ThemeContext";
+// import ThemeContext from './Context/ThemeContext'
+//eimport Header from './components/Header';
+export const ThemeContext = createContext('dark');
 
 function App() {
+  const [count, setCount] = useState(0);
   const [theme, setTheme] = useState("light");
-
+  useEffect(() => {
+    setTheme(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
+    );
+  }, []);
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={`${theme} ${theme == "dark" ? "bg-[#121212]" : null}`}>
