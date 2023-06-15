204a0f44368ad4e1be93a98ea5a241301305a644
HossainRafi <hossainrafi71@gmail.com>
2023-06-15 21:05:53 +0600

    new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <div className="grid grid-cols-4 p-8">
-      <div className="hidden md:flex">Genre</div>
+      <div className="hidden md:flex">
+        
+      </div>
       <div className="md:col-span-3 col-span-4 px-3">Game List</div>
     </div>
   );
