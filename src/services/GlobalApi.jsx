4ac1ce49941b32ea6da677371e796ea0c414ddaa
HossainRafi <hossainrafi71@gmail.com>
2023-06-15 22:15:17 +0600

    clear comment

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -2,9 +2,6 @@ import axios from "axios";
 const key = "795f0ecec0a64ec3825ec12593b566b3";
 const axioCreate = axios.create({
   baseURL: "https://api.rawg.io/api",
-  // params: {
-  //   key: "18d8dc115d954615a6fe8522598e8a97",
-  // },
 });
 
 const getPopularGame = axioCreate.get("/games?key=" + key);
