c5d67a1a8ee50ca0d5611a43f8fc1db6210102ba
HossainRafi <hossainrafi71@gmail.com>
2023-06-15 21:53:53 +0600

    globalapi

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,10 +1,18 @@
 import axios from "axios";
-
 const key = "795f0ecec0a64ec3825ec12593b566b3";
 const axioCreate = axios.create({
   baseURL: "https://api.rawg.io/api",
+  // params: {
+  //   key: "18d8dc115d954615a6fe8522598e8a97",
+  // },
 });
 
-const getGenereList = axioCreate.get("/genres?key=" + key);
-
-export default { getGenereList };
+const getPopularGame = axioCreate.get("/games?key=" + key);
+const getMovieDetails = (id) => axioCreate.get("/games/" + id + "/movies");
+const getGameListByGenreId = (id) =>
+  axioCreate.get("/games?key=" + key + "&genres=" + id);
+export default {
+  getPopularGame,
+  getMovieDetails,
+  getGameListByGenreId,
+};
