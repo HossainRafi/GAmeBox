ec3e3840f66ad1a4a5be365602e309dca182fb95
HossainRafi <hossainrafi71@gmail.com>
2023-06-15 20:45:02 +0600

    design child div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-
 function Home() {
   return (
     <div className="grid grid-cols-4 p-8">
-      <div>Genre</div>
-      <div>Game List</div>
+      <div className="hidden md:flex">Genre</div>
+      <div className="md:col-span-3 col-span-4 px-3">Game List</div>
     </div>
   );
 }
 
-export default Home
+export default Home;
